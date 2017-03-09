166684e55ff29b8ac10d78ebb4570a1d8ecd5f65
metoak-haosou <bobwangjie@163.com>
2017-03-10 00:56:05 +0800

    3.10

---
+++ led8/led8.ino
@@ -0,0 +1,24 @@
+/*
+*呼吸灯，利用正弦函数
+*
+*/
+
+byte led = 13;
+float sinval;
+int ledval;
+int i;
+void setup()
+{
+	pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+	for(i=0;i<180;i++){
+		sinval=sin(i*(3.1415/180));
+		ledval=sinval*255;
+		analogWrite(led,ledval);
+		delay(20);
+	} 
+
+}
---
+++ led9/led9.ino
@@ -0,0 +1,30 @@
+/*
+*彩虹灯
+*
+*/
+
+byte red = A1;
+byte green = A2;
+byte blue = A3;
+float sinval;
+int ledval;
+int i;
+void setup()
+{
+	pinMode(red,OUTPUT);
+	pinMode(green,OUTPUT);
+	pinMode(blue,OUTPUT);
+}
+
+void loop()
+{
+	byte i,j,k;
+	for(i=0;i<255;i++)
+		for(j=0;j<255;j++)
+			for(k=0;k<255;k++){
+				analogWrite(red,i);
+				analogWrite(green,j);
+				analogWrite(blue,k);
+				delay(20);
+			}
+}
